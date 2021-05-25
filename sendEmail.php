8e9c15478f6b75630f91122b54a9d33ba25cf19e
sultan-710 <73881369+sultan-710@users.noreply.github.com>
2021-05-25 22:52:55 +0200

    Add files via upload

---
+++ sendEmail.php
@@ -0,0 +1,24 @@
+﻿<?php
+
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = '2135879@talnet.nl';
+
+$email_subject = 'New form Submission';
+
+$email_body = "User Name: $name.\n".
+               "User Email: $visitor_email.\n".
+               "Subject: $subject.\n".
+               "User Message: $message .\n".;
+
+$to = '2135879@talnet.nl';
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To; $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+header("Location: inschrijven.html");
+?>
